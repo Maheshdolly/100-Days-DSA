48b5e7919dce9b2a69bab3c8bca9d8f562240f80
V Mahesh kumar <144375605+Maheshdolly@users.noreply.github.com>
2025-01-20 16:25:16 +0530

    Time: 2 ms (97.23%), Space: 55.9 MB (11.07%) - LeetHub

---
+++ 1372-check-if-it-is-a-good-array/1372-check-if-it-is-a-good-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isGoodArray(int[] nums) {
+        int result = nums[0];
+        for(int i=0; i<nums.length; i++){
+            result = gcd(result, nums[i]);
+        }
+        return result == 1;
+    }
+    private int gcd(int a , int b){
+        if(a==0)
+        return b;
+        else
+        return gcd(b%a,a);
+    }
+}
